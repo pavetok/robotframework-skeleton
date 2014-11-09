d50cb8c60dc7cd49aaa3f96e61abde0b755a390c
pavetok <p.vetokhin@gmail.com>
2014-11-09 08:43:00 +0300

    experiments with fig and ansible

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from fabric.api import local
+
+
+def deploy(stand_name):
+    local("cp stands/{0}/* .".format(stand_name))
+    local("fig up")
