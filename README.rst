b3e64eba77299312b76586c5a4a4ff3c11922052
pavetok <p.vetokhin@gmail.com>
2014-11-09 11:38:11 +0300

    added readme

---
+++ README.rst
@@ -0,0 +1,15 @@
+About
+-----
+Skeleton for the functional tests on the Robot Framework.
+
+Usage
+-----
+::
+
+    git clone https://github.com/pavetok/robotframework-skeleton.git tests
+    cd tests
+    virtualenv venv  # optional
+    source venv/bin/activate  # optional
+    pip install -U install -r host-requirements.txt
+    cp stands/{stand_name} .
+    fig up
--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -3,14 +3,6 @@ from __future__ import unicode_literals
 from fabric.api import local
 
 
-# def deploy(stand_name):
-#     local("cp stands/{0}/Dockerfile .".format(stand_name))
-#     local("cp stands/{0}/.dockerignore .".format(stand_name))
-#     local("ansible-playbook ansible/playbook.yml")
-    # local("fig up")
-
-
 def deploy(stand_name):
-    local("cp stands/common/guest-requirements.txt stands/{0}".format(stand_name))
-    local("ansible-playbook stands/{0}/playbook.yml".format(stand_name))
-    # local("ansible-playbook ansible/playbook.yml")
+    local("cp stands/{0}/* .".format(stand_name))
+    local("fig up")
