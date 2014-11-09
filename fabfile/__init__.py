e2c912521253e41eb9e3fbcf9bc81c833798e166
pavetok <p.vetokhin@gmail.com>
2014-11-09 09:38:53 +0300

    experiments with fig and ansible: copy and run with fabric

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -4,5 +4,7 @@ from fabric.api import local
 
 
 def deploy(stand_name):
-    local("cp stands/{0}/* .".format(stand_name))
-    local("fig up")
+    local("cp stands/{0}/Dockerfile .".format(stand_name))
+    local("cp stands/{0}/.dockerignore .".format(stand_name))
+    local("ansible-playbook ansible/playbook.yml")
+    # local("fig up")
